82ee75173e9cd41b6efd2061b70170da2a80172e
stephenkati <stephenkatuli88@gmail.com>
2023-04-25 12:41:09 +0300

    import FetchQuote component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import FetchQuote from './api/Quote';
 import Calculator from './components/Calculator';
 
 const App = () => (
   <div className="app">
+    <FetchQuote />
     <Calculator />
   </div>
 );
