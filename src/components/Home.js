8365768b4d21abb77391ddaacc5687881631876e
stephenkati <stephenkatuli88@gmail.com>
2023-05-01 13:33:57 +0300

    create Home.js file

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+const Home = () => (
+  <>
+    <div className="homeContent">
+      <p>Hello and Welcome to this calculator webapp.</p>
+      <p>
+        In the calculator page you will find the calculator application, have fun with some math
+      </p>
+      <p>In the quote page you will find some motivational, insightful quotes. Enjoy</p>
+    </div>
+  </>
+);
+
+export default Home;
